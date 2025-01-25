d2c0d4d79a96c3e2ca67729864e3bc02f287a3e6
Xharry01X <harshitsingh50621@gmail.com>
2025-01-26 01:16:44 +0530

    learning match

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+fn main() {
+   
+  let fruit = "banana";
+
+  match fruit {
+    "apple" => println!("put inside apple box"),
+    "banana" => println!("put it inside banana box"),
+    _=> println!("I dont know where this goes"),
+  }
+
+}
